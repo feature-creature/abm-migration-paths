8d13ee3d2136bd45697270d4b0430babf6988a84
feature-creature <jdema003@gold.ac.uk>
2020-04-01 03:53:35 +0100

    adds rough abm data logging and descriptives plotting

--- sketch-abm.js
+++ sketch-abm.js
@@ -6,15 +6,18 @@
 let migrationPathwaysABM = p5i => {
   p5i.disableFriendlyErrors = true;
 
-  var pause = false;
-  var fr = 30;
-  var wEnv = 600; //1920
-  var hEnv = 600; //1080
-  p5i.ticks = 300;
+  var pause = true;
+  var fr = 60;
+  var wEnv = 1920; //1920
+  var hEnv = 1080; //1080
+  p5i.ticks = 1000;
   
 
-  var numOfMigrants = 30;
+  var numOfMigrants = 150;
   p5i.migrants = [];
+
+  p5i.logMigrantStates = [];
+
   p5i.origin = [[0,0],[wEnv/2,hEnv]];
   p5i.destination = [[wEnv/2,0],[wEnv,hEnv]];
 
@@ -31,13 +34,15 @@ let migrationPathwaysABM = p5i => {
         new Migrant(p5i, p5i.random(p5i.origin[0][0]+15, p5i.origin[1][0]), p5i.random(p5i.origin[0][1]+15, p5i.origin[1][1]-15), 0, 0, 15)
       );
     };
+    p5i.noLoop();
   }
 
 
   p5i.draw = function() {
     p5i.drawEnvironment();
-    for(var i = 0; i < p5i.migrants.length; i++){p5i.migrants[i].show();}
     for(var i = 0; i < p5i.migrants.length; i++){p5i.migrants[i].update();}
+    for(var i = 0; i < p5i.migrants.length; i++){p5i.migrants[i].show();}
+    p5i.logStates();
     p5i.drawLabels();
     p5i.isCompleted();
   }
@@ -54,8 +59,8 @@ let migrationPathwaysABM = p5i => {
   p5i.drawLabels = function(){
     p5i.push();
     p5i.stroke(0);
-    p5i.text("Model",15,35);
-    p5i.text(p5i.frameCount + "/" + p5i.ticks,15,55);
+    p5i.text("Migrant Pathways",15,35);
+    //p5i.text(p5i.frameRate(),15,77);
     p5i.pop();
   }
 
@@ -65,7 +70,27 @@ let migrationPathwaysABM = p5i => {
     pause?p5i.noLoop():p5i.loop();
   }
 
+
   p5i.isCompleted = function(){if(p5i.frameCount>=p5i.ticks)p5i.noLoop();}
+
+
+  //--------------------------------
+
+
+  p5i.logStates = function(){
+    var numOfPotential = 0;
+    var numOfSeeking = 0;
+    var numOfTransit = 0;
+    var numOfEmployed = 0;
+    for(var i = 0; i < p5i.migrants.length; i++){
+      if(p5i.migrants[i].state == "potential")numOfPotential++;
+      if(p5i.migrants[i].state == "seeking")numOfSeeking++;
+      if(p5i.migrants[i].state == "transit")numOfTransit++;
+      if(p5i.migrants[i].state == "employed")numOfEmployed++;
+    }
+    p5i.logMigrantStates.push([numOfPotential,numOfSeeking,numOfTransit,numOfEmployed]);
+  }
+
 }
 
 
--- sketch-descriptives.js
+++ sketch-descriptives.js
@@ -8,23 +8,23 @@ let migrationPathwaysDescriptives = p5i => {
 
   var pause = false;
   var fr = 30;
-  var wEnv = 600; //1920
-  var hEnv = 200; //1080
+  var wEnv = 1920;
+  var hEnv = 200;
 
   p5i.setup = function() {
     var canvas = p5i.createCanvas(wEnv, hEnv,p5i.P2D);
-    //canvas.mousePressed(p5i.pauseSketch);
     p5i.frameRate(fr);
     p5i.textSize(22);
     p5i.background(175);
-
     p5i.drawEnvironment();
   }
 
 
   p5i.draw = function() {
     p5i.drawEnvironment();
+    p5i.drawTrendlines();
     p5i.drawLabels();
+    if(abm.logMigrantStates.length == abm.ticks)p5i.noLoop();
   }
 
 
@@ -37,11 +37,111 @@ let migrationPathwaysDescriptives = p5i => {
 
   p5i.drawLabels = function(){
     p5i.push();
-    p5i.stroke(0);
-    p5i.text("Data",15,35);
+      p5i.textAlign(p5i.LEFT);
+      p5i.translate(5,0);
+      p5i.stroke(0);
+      p5i.text("0",0,hEnv-7);
+      p5i.text(abm.migrants.length,0,hEnv-140);
+      p5i.textAlign(p5i.RIGHT);
+      p5i.text(abm.logMigrantStates.length +" / "+abm.ticks,wEnv-12,hEnv-7);
+    p5i.pop();
+    
+    p5i.push();
+      p5i.textAlign(p5i.RIGHT);
+      p5i.translate(0,25);
+      p5i.push();
+        p5i.translate(253,0);
+        p5i.text(abm.logMigrantStates[abm.logMigrantStates.length-1][0] + " Potential Migrants", 0, 0);
+        p5i.stroke(255);
+        p5i.fill(255);
+        p5i.ellipse(17,-7,15,15);
+      p5i.pop();
+      p5i.push();
+        p5i.translate(wEnv * 0.4,0);
+        p5i.text(abm.logMigrantStates[abm.logMigrantStates.length-1][1] + " Seeking Migrants", 0, 0);
+        p5i.strokeWeight(3);
+        p5i.stroke(255);
+        p5i.fill(255,174,66);
+        p5i.ellipse(17,-7,15,15);
+      p5i.pop();
+      p5i.push();
+        p5i.translate(wEnv * 0.7,0);
+        p5i.text(abm.logMigrantStates[abm.logMigrantStates.length-1][2] + " Transit Migrants", 0, 0);
+        p5i.strokeWeight(3);
+        p5i.stroke(200,255,255);
+        p5i.fill(255,174,66);
+        p5i.line(17, -7, 37, -7);
+        p5i.ellipse(17,-7,15,15);
+      p5i.pop();
+      p5i.push();
+        p5i.translate(wEnv - 35,0);
+        p5i.text(abm.logMigrantStates[abm.logMigrantStates.length-1][3] + " Employed Migrants", 0, 0);
+        p5i.strokeWeight(3);
+        p5i.stroke(0,0,255);
+        p5i.fill(255,174,66);
+        p5i.ellipse(17,-7,15,15);
+      p5i.pop();
     p5i.pop();
   }
 
+
+  p5i.drawTrendlines = function(){
+    p5i.push();
+      p5i.translate(10,hEnv-35);
+      //---
+      p5i.push();
+        p5i.fill(abm.migrants[0].states.employed.stroke);
+        p5i.noStroke();
+        p5i.beginShape();
+        p5i.vertex(0,-95);
+        for(var i = 0; i < abm.logMigrantStates.length;i++){
+          p5i.vertex((wEnv-20)/abm.ticks*(i+1), p5i.map(abm.logMigrantStates[i][0],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][1],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][2],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][3],0,abm.migrants.length,0,-95));
+        }
+        p5i.vertex((wEnv-20)/abm.ticks*(i), 0);
+        p5i.vertex(0,0);
+        p5i.endShape(p5i.CLOSE);
+      p5i.pop();
+      //---
+      p5i.push();
+        p5i.fill(abm.migrants[0].states.transit.stroke);
+        p5i.noStroke();
+        p5i.beginShape();
+        p5i.vertex(0,-95);
+        for(var i = 0; i < abm.logMigrantStates.length;i++){
+          p5i.vertex((wEnv-20)/abm.ticks*(i+1), p5i.map(abm.logMigrantStates[i][0],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][1],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][2],0,abm.migrants.length,0,-95));
+        }
+        p5i.vertex((wEnv-20)/abm.ticks*(i), 0);
+        p5i.vertex(0,0);
+        p5i.endShape(p5i.CLOSE);
+      p5i.pop();
+      //---
+        p5i.push();
+        p5i.fill(abm.migrants[0].states.seeking.color);
+        p5i.noStroke();
+        p5i.beginShape();
+        p5i.vertex(0,-95);
+        for(var i = 0; i < abm.logMigrantStates.length;i++){
+          p5i.vertex((wEnv-20)/abm.ticks*(i+1), p5i.map(abm.logMigrantStates[i][1],0,abm.migrants.length,0,-95) + p5i.map(abm.logMigrantStates[i][0],0,abm.migrants.length,0,-95));
+        }
+        p5i.vertex((wEnv-20)/abm.ticks*(i), 0);
+        p5i.vertex(0,0);
+        p5i.endShape(p5i.CLOSE);
+      p5i.pop();
+      //---
+      p5i.push();
+        p5i.fill(abm.migrants[0].states.potential.color);
+        p5i.noStroke();
+        p5i.beginShape();
+        p5i.vertex(0,-95);
+        for(var i = 0; i < abm.logMigrantStates.length;i++){
+          p5i.vertex((wEnv-20)/abm.ticks*(i+1), p5i.map(abm.logMigrantStates[i][0],0,abm.migrants.length,0,-95));
+        }
+        p5i.vertex((wEnv-20)/abm.ticks*(i), 0);
+        p5i.vertex(0,0);
+        p5i.endShape(p5i.CLOSE);
+      p5i.pop();
+    p5i.pop();
+  }
 
   p5i.pauseSketch = function(){
     pause = pause ? false : true;
